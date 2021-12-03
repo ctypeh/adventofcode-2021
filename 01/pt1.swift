b76f52d67f880e63dd6d61c0e1e57da9b6a6feb0
Claire Violet <code@claire.jetzt>
2021-12-02 21:23:56 -0600

    Day 1, Part 1, Revision 2

--- 01/pt1.swift
+++ 01/pt1.swift
@@ -7,3 +7,28 @@
 
 import Foundation
 
+var depthIncreases = 0, prev, next: Int
+
+var input : String?
+
+//read first value
+input = readLine() ?? ""
+prev = Int(input!) ?? 0
+//read second value
+input = readLine() ?? ""
+
+while(input != nil) {
+    //parse int into next
+    next = Int(input!) ?? 0
+    
+    if(prev < next ) {
+        depthIncreases += 1
+    }
+    
+    //read next input
+    input = readLine()
+    //move forward
+    prev = next
+}
+
+print(depthIncreases)
